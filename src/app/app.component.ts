479abb14495a36c18f02a7d8932ec66fd055e41b
Kimmo Halonen <Kimmo Halonen>
2017-04-30 22:46:36 +0300

    Recipe book elements hooked up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  toolbarElement = "recipies";
 
+
+  onToolbarSelect(feature: string) {
+    this.toolbarElement = feature;
+  }
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipies: Recipe[] = [
-    new Recipe('A test recipe','This is simply a test', 'https://c1.staticflickr.com/3/2785/4106965666_79f19f168c.jpg' )
+    new Recipe('A test recipe','This is simply a test', 'https://c1.staticflickr.com/3/2785/4106965666_79f19f168c.jpg' ),
+    new Recipe('A test recipe 2','This is second test', 'https://c1.staticflickr.com/3/2785/4106965666_79f19f168c.jpg' )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
